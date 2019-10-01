8b02652a5b04f1fa4e0e0277e004e082501b79e3
joak32 <oakxx011@umn.edu>
2019-10-01 11:26:05 -0500

    Added a translation and quote to geeksay.js

--- geeksay.js
+++ geeksay.js
@@ -82,7 +82,8 @@ var translations = {
     "universe": "42",
     "everything": "42",
     "lifetime": "runtime",
-    "sleep": "shutdown"
+    "sleep": "shutdown",
+    "send" : "push"
 }
 
 var quotes = [
@@ -112,7 +113,8 @@ var quotes = [
     "he wore black and i wore white",
     "bang bang my baby shot me down",
     "Today is the first day of the rest of your lifetime",
-    "i need to sleep"
+    "i need to sleep",
+    "Looks good to me!"
 ]
 
 function isNumeric(num){
