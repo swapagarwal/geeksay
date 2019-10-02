e3cc9ed1f049d2a85465786d0a20a39db11514d7
Elton <elton@moovin.com.br>
2019-10-02 20:31:06 -0300

    new quote

--- geeksay.js
+++ geeksay.js
@@ -142,6 +142,7 @@ const quotes = [
     "eat sleep code repeat", // eat shutdown code repeat
     "roses are red, violets are blue", // roses are ##ff0000, violets are ##0000ff
     "The greatest trick the devil ever pulled was convincing the world he didn't exist", // The greatest trick the devil ever pulled was convincing the 0.0.0.0/0 he didn't exist 
+    "Home sweet home", // 127.0.0.1 sweet 127.0.0.1
 ]
 
 function isNumeric(num){
