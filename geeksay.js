fad111f701a1d4f086af48bddc39bd192b45f642
Roberto R <zenithpolar@users.noreply.github.com>
2019-10-07 12:03:42 -0300

    Add Quote to geeksay.js
    This quote is by Peter Sunde, one of TPB founders.

--- geeksay.js
+++ geeksay.js
@@ -142,6 +142,7 @@ const quotes = [
     "eat sleep code repeat", // eat shutdown code repeat
     "roses are red, violets are blue", // roses are ##ff0000, violets are ##0000ff
     "The greatest trick the devil ever pulled was convincing the world he didn't exist", // The greatest trick the devil ever pulled was convincing the 0.0.0.0/0 he didn't exist
+    "We don’t use the expression IRL. We say AFK - Away from keyboard. We think the internet is for real." //TPB AFK
 ]
 
 function isNumeric(num){
