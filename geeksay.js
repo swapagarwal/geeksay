21f73f0bfdd0f416900a525b285492c2c5ed2946
Sarah Waldie <swaldie@Sarahs-MacBook-Pro-3.local>
2019-10-09 17:42:02 -0400

    refactor: break out function to get text and update with ternaries

--- geeksay.js
+++ geeksay.js
@@ -1,182 +1,178 @@
 const translations = {
-    "save": "ctrl+s",
-    "break": "<br>",
-    "control": "ctrl",
-    "rule": "ctrl",
-    "escape": "esc",
-    "black": "#000",
-    "white": "#fff",
-    "bang": "!",
-    "not": "!",
-    "new": "ctrl+n",
-    "open": "ctrl+o",
-    "anchor": "<a>",
-    "bold": "<b>",
-    "cut": "ctrl+x",
-    "copy": "ctrl+c",
-    "paste": "ctrl+v",
-    "quit": "ctrl+q",
-    "close": "ctrl+w",
-    "help": "f1",
-    "rename": "f2",
-    "undo": "ctrl+z",
-    "redo": "ctrl+y",
-    "search": "ctrl+f",
-    "find": "ctrl+f",
-    "replace": "ctrl+r",
-    "print": "ctrl+p",
-    "get": "git",
-    "got": "git",
-    "home": "127.0.0.1",
-    "house": "127.0.0.1",
-    "local": "127.0.0.1",
-    "localhost": "127.0.0.1",
-    "fuck": "fork",
-    "hello": "ping",
-    "hi": "ping",
-    "hey": "ping",
-    "command": "cmd",
-    "forget": "ctrl+z",
-    "world": "0.0.0.0/0",
-    "want": "require",
-    "name": "alias",
-    "address": "url",
-    "pie": "π",
-    "function": "fn",
-    "kill": "ctrl+c",
-    "stop": "abort",
-    "refresh": "f5",
-    "slow": "O(n^n)",
-    "fast": "O(1)",
-    "leet": "1337",
-    "hacker": "haxor",
-    "geek": "g33k",
-    "plus": "+",
-    "minus": "-",
-    "and": "&&",
-    "or": "||",
-    "binary": "01",
-    "call": "ping",
-    "me": "self",
-    "owner": "admin",
-    "god": "root",
-    "please": "sudo",
-    "at": "@",
-    "fix": "debug",
-    "shit": "dump",
-    "bullshit": "dump",
-    "poop": "dump",
-    "delete": "del",
-    "force": "ma",
-    "coffee": "covfefe",
-    "number": "#",
-    "image": "img",
-    "photo": "img",
-    "pic": "img",
-    "picture": "img",
-    "snap": "img",
-    "snapshot": "img",
-    "bye": "exit",
-    "noob": "n00b",
-    "life": "42",
-    "universe": "42",
-    "everything": "42",
-    "lifetime": "runtime",
-    "sleep": "shutdown",
-    "red": "#ff0000",
-    "green": "#00ff00",
-    "blue": "#0000ff",
-    "marry": "merge",
-    "propose": "commit",
-    "not found": "404",
-    "link": "url",
-    "forbidden": "403",
-    "send" : "push",
-    "alternate": "alt",
-    "alternative": "alt",
-    "inspect": "ctrl+shift+i",
-    "equal": "=",
-    "remove": "rm",
-    "move": "mv",
-    "plus": "+",
-    "random": "rand",
-    "mathematics": "math",
-    "heart": "<3",
-    "love": "<3",
-    "like": "<3",
-}
+  save: "ctrl+s",
+  break: "<br>",
+  control: "ctrl",
+  rule: "ctrl",
+  escape: "esc",
+  black: "#000",
+  white: "#fff",
+  bang: "!",
+  not: "!",
+  new: "ctrl+n",
+  open: "ctrl+o",
+  anchor: "<a>",
+  bold: "<b>",
+  cut: "ctrl+x",
+  copy: "ctrl+c",
+  paste: "ctrl+v",
+  quit: "ctrl+q",
+  close: "ctrl+w",
+  help: "f1",
+  rename: "f2",
+  undo: "ctrl+z",
+  redo: "ctrl+y",
+  search: "ctrl+f",
+  find: "ctrl+f",
+  replace: "ctrl+r",
+  print: "ctrl+p",
+  get: "git",
+  got: "git",
+  home: "127.0.0.1",
+  house: "127.0.0.1",
+  local: "127.0.0.1",
+  localhost: "127.0.0.1",
+  fuck: "fork",
+  hello: "ping",
+  hi: "ping",
+  hey: "ping",
+  command: "cmd",
+  forget: "ctrl+z",
+  world: "0.0.0.0/0",
+  want: "require",
+  name: "alias",
+  address: "url",
+  pie: "π",
+  function: "fn",
+  kill: "ctrl+c",
+  stop: "abort",
+  refresh: "f5",
+  slow: "O(n^n)",
+  fast: "O(1)",
+  leet: "1337",
+  hacker: "haxor",
+  geek: "g33k",
+  plus: "+",
+  minus: "-",
+  and: "&&",
+  or: "||",
+  binary: "01",
+  call: "ping",
+  me: "self",
+  owner: "admin",
+  god: "root",
+  please: "sudo",
+  at: "@",
+  fix: "debug",
+  shit: "dump",
+  bullshit: "dump",
+  poop: "dump",
+  delete: "del",
+  force: "ma",
+  coffee: "covfefe",
+  number: "#",
+  image: "img",
+  photo: "img",
+  pic: "img",
+  picture: "img",
+  snap: "img",
+  snapshot: "img",
+  bye: "exit",
+  noob: "n00b",
+  life: "42",
+  universe: "42",
+  everything: "42",
+  lifetime: "runtime",
+  sleep: "shutdown",
+  red: "#ff0000",
+  green: "#00ff00",
+  blue: "#0000ff",
+  marry: "merge",
+  propose: "commit",
+  "not found": "404",
+  link: "url",
+  forbidden: "403",
+  send: "push",
+  alternate: "alt",
+  alternative: "alt",
+  inspect: "ctrl+shift+i",
+  equal: "=",
+  remove: "rm",
+  move: "mv",
+  plus: "+",
+  random: "rand",
+  mathematics: "math",
+  heart: "<3",
+  love: "<3",
+  like: "<3"
+};
 
 const quotes = [
-    "Hello World", // ping 0.0.0.0/0
-    "Bye World", // exit 0.0.0.0/0
-    "Forget that ever happened", // ctrl+z that ever happened
-    "Home is where the heart is", // 127.0.0.1 is where the heart is
-    "Home is where ideas come to life", // 127.0.0.1 is where ideas come to 42
-    "Home is where the wifi connects automatically", // 127.0.0.1 is where the wifi connects automatically
-    "fuck that shit", // fork that dump
-    "go hard or go home", // go hard || go 127.0.0.1
-    "I will call you", // I will ping you
-    "I want the world to know me", // I require the 0.0.0.0/0 to know self
-    "I am the owner", // I am the admin
-    "I am God", // I am root
-    "please make me a sandwich", // sudo make self a sandwich
-    "coffee is life", // covfefe is 42
-    "what's your name", // what's your alias
-    "I will find you and I will fix you!", // I will ctrl+f you && I will debug you!
-    "geek alert", // g33k alert
-    "geek inside", // g33k inside
-    "copy that!", // ctrl+c that!
-    "get out of home", // git out of 127.0.0.1
-    "not everything is black and white", // ! 42 is #000 && #fff
-    "to be or not to be, that is the question", // to be || ! to be, that is the question
-    "i was 5 and he was 6", // i was 101 && he was 110
-    "he wore black and i wore white", // he wore #000 && i wore #fff
-    "bang bang my baby shot me down", // ! ! my baby shot self down
-    "Today is the first day of the rest of your lifetime", // Today is the first day of the rest of your runtime
-    "i need to sleep", // i need to shutdown
-    "There are ten types of people in the world: those who understand binary and those who don't", // There are ten types of people in the world: those who understand 01 && those who don't
-    "I love my life", // I <3 my 42
-    "I love my house", // I <3 my 127.0.0.1
-    "heart break", // <3 <br>
-    "I like to fix shit", // I <3 to debug dump
-    "eat sleep code repeat", // eat shutdown code repeat
-    "roses are red, violets are blue", // roses are ##ff0000, violets are ##0000ff
-    "The greatest trick the devil ever pulled was convincing the world he didn't exist", // The greatest trick the devil ever pulled was convincing the 0.0.0.0/0 he didn't exist 
-    "home sweet home", // 127.0.0.1 sweet 127.0.0.1
-]
+  "Hello World", // ping 0.0.0.0/0
+  "Bye World", // exit 0.0.0.0/0
+  "Forget that ever happened", // ctrl+z that ever happened
+  "Home is where the heart is", // 127.0.0.1 is where the heart is
+  "Home is where ideas come to life", // 127.0.0.1 is where ideas come to 42
+  "Home is where the wifi connects automatically", // 127.0.0.1 is where the wifi connects automatically
+  "fuck that shit", // fork that dump
+  "go hard or go home", // go hard || go 127.0.0.1
+  "I will call you", // I will ping you
+  "I want the world to know me", // I require the 0.0.0.0/0 to know self
+  "I am the owner", // I am the admin
+  "I am God", // I am root
+  "please make me a sandwich", // sudo make self a sandwich
+  "coffee is life", // covfefe is 42
+  "what's your name", // what's your alias
+  "I will find you and I will fix you!", // I will ctrl+f you && I will debug you!
+  "geek alert", // g33k alert
+  "geek inside", // g33k inside
+  "copy that!", // ctrl+c that!
+  "get out of home", // git out of 127.0.0.1
+  "not everything is black and white", // ! 42 is #000 && #fff
+  "to be or not to be, that is the question", // to be || ! to be, that is the question
+  "i was 5 and he was 6", // i was 101 && he was 110
+  "he wore black and i wore white", // he wore #000 && i wore #fff
+  "bang bang my baby shot me down", // ! ! my baby shot self down
+  "Today is the first day of the rest of your lifetime", // Today is the first day of the rest of your runtime
+  "i need to sleep", // i need to shutdown
+  "There are ten types of people in the world: those who understand binary and those who don't", // There are ten types of people in the world: those who understand 01 && those who don't
+  "I love my life", // I <3 my 42
+  "I love my house", // I <3 my 127.0.0.1
+  "heart break", // <3 <br>
+  "I like to fix shit", // I <3 to debug dump
+  "eat sleep code repeat", // eat shutdown code repeat
+  "roses are red, violets are blue", // roses are ##ff0000, violets are ##0000ff
+  "The greatest trick the devil ever pulled was convincing the world he didn't exist", // The greatest trick the devil ever pulled was convincing the 0.0.0.0/0 he didn't exist
+  "home sweet home" // 127.0.0.1 sweet 127.0.0.1
+];
 
-function isNumeric(num){
-    return !isNaN(parseInt(num));
+function isNumeric(num) {
+  return !isNaN(parseInt(num));
+}
+
+function getGeeksayText(text) {
+  const lowerCaseText = text.toLowerCase();
+  return translations.hasOwnProperty(lowerCaseText)
+    ? translations[lowerCaseText]
+    : text;
+}
+
+function getGeeksayWord(text) {
+  return isNumeric(text) ? (text >>> 0).toString(2) : getGeeksayText(text);
 }
 
 function geeksay(text) {
-    const input =  Array.isArray(text) ? text : String(text).split(' ');
-
-    return input.map(geeksayWord).join(' ');
-}
-
-function geeksayWord(text) {
-    if(isNumeric(text)) {
-        return (text >>> 0).toString(2);
-    }
-    else {
-        lowerCaseText = text.toLowerCase();
-        if (translations.hasOwnProperty(lowerCaseText)) {
-            return translations[lowerCaseText];
-        } else {
-            return text;
-        }
-    }
+  const input = Array.isArray(text) ? text : String(text).split(" ");
+  return input.map(getGeeksayWord).join(" ");
 }
 
 function getRandomTranslation() {
-    const keys = Object.keys(translations);
-    const randomKey = keys[Math.floor(Math.random() * keys.length)];
-    return "Random Translation: " + randomKey + " -> " + translations[randomKey];
+  const keys = Object.keys(translations);
+  const randomKey = keys[Math.floor(Math.random() * keys.length)];
+  return "Random Translation: " + randomKey + " -> " + translations[randomKey];
 }
 
 function getRandomQuote() {
-    return quotes[Math.floor(Math.random() * quotes.length)];
+  return quotes[Math.floor(Math.random() * quotes.length)];
 }
 
 module.exports = geeksay;
