fedf46a45a2088f8d4f628c1f2892dc2fd635742
Courtney Ivey <oxocodes@gmail.com>
2019-10-12 13:38:17 -0700

    Added Fix it, Jesus

--- geeksay.js
+++ geeksay.js
@@ -143,6 +143,7 @@ const quotes = [
     "roses are red, violets are blue", // roses are ##ff0000, violets are ##0000ff
     "The greatest trick the devil ever pulled was convincing the world he didn't exist", // The greatest trick the devil ever pulled was convincing the 0.0.0.0/0 he didn't exist 
     "home sweet home", // 127.0.0.1 sweet 127.0.0.1
+    "fix it Jesus" // debug it Jesus
 ]
 
 function isNumeric(num){
