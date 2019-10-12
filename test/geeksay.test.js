7fd66762be3590b49afdc4452c901b2af28275cd
Swapnil Agarwal <2833845+swapagarwal@users.noreply.github.com>
2019-10-12 20:04:32 +0530

    Update geeksay.test.js

--- test/geeksay.test.js
+++ test/geeksay.test.js
@@ -4,17 +4,17 @@ const geeksay = require('../geeksay')
 describe('words', () => {
   it('love', () => {
     should.equal(geeksay('love'), '<3');
-    });
-    
-    it('not', () => {
+  });
+
+  it('not', () => {
     should.equal(geeksay('not'), '!');
-    });
-    
-    it('slow', () => {
+  });
+
+  it('slow', () => {
     should.equal(geeksay('slow'), 'O(n^n)');
-    });
-    
-    it('SloW - case insensitive', () => {
+  });
+
+  it('SloW - case insensitive', () => {
     should.equal(geeksay('SloW'), 'O(n^n)');
   });
 });
@@ -23,76 +23,76 @@ describe('words', () => {
 describe('quotes', () => {
   it('Hello World', () => {
     should.equal(geeksay('Hello World'), 'ping 0.0.0.0/0');
-    });
+  });
 
-    it('heLLo WorlD - case insensitive', () => {
+  it('heLLo WorlD - case insensitive', () => {
     should.equal(geeksay('heLLo WorlD'), 'ping 0.0.0.0/0');
-    });
+  });
 
-    it('I love my house', () => {
+  it('I love my house', () => {
     should.equal(geeksay('I love my house'), 'I <3 my 127.0.0.1');
-    });
+  });
 });
 
 describe('multi-line and symbols', () => {
-    it(`Every day after waking up \\n I say Hello World and \\n hi \\n to the world `, () => {
+  it(`Every day after waking up \\n I say Hello World and \\n hi \\n to the world `, () => {
     should.equal(geeksay(`Every day after waking up
-        I say Hello World and 
+        I say Hello World and
         hi
         to the world `), `Every day after waking up
-        I say ping 0.0.0.0/0 && 
+        I say ping 0.0.0.0/0 &&
         hi
         to the 0.0.0.0/0 `);
-    });
+  });
 
-    it('space', () => {
+  it('space', () => {
     should.equal(geeksay('I love my            house'), 'I <3 my            127.0.0.1');
-    });
+  });
 
-    it('symbols #1', () => {
+  it('symbols #1', () => {
     should.equal(geeksay('I love my @ house'), 'I <3 my @ 127.0.0.1');
-    });
+  });
 
-    it('symbols #2', () => {
+  it('symbols #2', () => {
     should.equal(geeksay('(=> I love my house )'), '(=> I <3 my 127.0.0.1 )');
-    });
+  });
 
-    // TODO: 
-    // it('symbols #3', () => {
+  // TODO:
+  // it('symbols #3', () => {
   // 	should.equal(geeksay('I love my house!'), 'I <3 my 127.0.0.1!');
-    // });
+  // });
 
 });
 
 describe('html tags', () => {
   it('inside tag with space', () => {
     should.equal(geeksay('<a href="#"> Hello World </a>'), '<a href="#"> ping 0.0.0.0/0 </a>');
-    });
+  });
 
-    // TODO: 
-    // it('inside tag without space', () => {
+  // TODO:
+  // it('inside tag without space', () => {
   // 	should.equal(geeksay('<a href="#">Hello World</a>'), '<a href="#">ping 0.0.0.0/0</a>');
-    // });
+  // });
 });
 
 describe('inputs', () => {
   it('input as array', () => {
     should.equal(geeksay(['hello', 'my', 'love']), 'ping my <3');
-    });
+  });
 
-    it('null', () => {
+  it('null', () => {
     should.equal(geeksay(null), 'null');
-    });
+  });
 
-    it('undefined', () => {
+  it('undefined', () => {
     should.equal(geeksay(undefined), 'undefined');
-    });
+  });
 
-    it('object', () => {
-    should.equal(geeksay( {Hello: 'World'} ), '[object Object]');
-    });
+  it('object', () => {
+    should.equal(geeksay({ Hello: 'World' }), '[object Object]');
+  });
 
-    it('array of numbers', () => {
-    should.equal(geeksay( [1, 2, 3, 4, '100'] ), '1 10 11 100 1100100');
-    });
+  it('array of numbers', () => {
+    should.equal(geeksay([1, 2, 3, 4, '100']), '1 10 11 100 1100100');
+  });
 });
