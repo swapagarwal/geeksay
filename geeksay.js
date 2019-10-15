d783464f01a6a6fbc68fe08968ed8613ecfd32c4
Jyothish Kumar M S <jeevanms15@gmail.com>
2019-10-16 00:39:18 +0530

    Added quote

--- geeksay.js
+++ geeksay.js
@@ -102,6 +102,7 @@ const translations = {
     "mathematics": "math",
     "heart": "<3",
     "love": "<3",
+    "style": "<style>",
 }
 
 const quotes = [
@@ -141,6 +142,7 @@ const quotes = [
     "roses are red, violets are blue", // roses are ##ff0000, violets are ##0000ff
     "The greatest trick the devil ever pulled was convincing the world he didn't exist", // The greatest trick the devil ever pulled was convincing the 0.0.0.0/0 he didn't exist 
     "home sweet home", // 127.0.0.1 sweet 127.0.0.1
+    "Web developers do it with style", // Web developers do it with <style>
 ]
 
 function isNumeric(num){
