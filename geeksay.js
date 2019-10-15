ac988df1d1a9bc8eb2ca266fac484d21f073a8a9
Abel Berhane <abelberhane@gmail.com>
2019-10-15 13:51:50 -0400

    added 2 entries

--- geeksay.js
+++ geeksay.js
@@ -102,6 +102,7 @@ const translations = {
     "mathematics": "math",
     "heart": "<3",
     "love": "<3",
+    "idiot": "ID10T",
 }
 
 const quotes = [
@@ -141,6 +142,7 @@ const quotes = [
     "roses are red, violets are blue", // roses are ##ff0000, violets are ##0000ff
     "The greatest trick the devil ever pulled was convincing the world he didn't exist", // The greatest trick the devil ever pulled was convincing the 0.0.0.0/0 he didn't exist 
     "home sweet home", // 127.0.0.1 sweet 127.0.0.1
+    "I break warranties". // I <br> warranties
 ]
 
 function isNumeric(num){
