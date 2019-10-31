6098c4ae6b51ff763e066b5160d20e807759cbda
Tyrilanel <an-monteil@hotmail.fr>
2019-10-31 23:54:31 +0100

    add translations for important and object

--- geeksay.js
+++ geeksay.js
@@ -121,6 +121,9 @@ const translations = {
     "waiting": "loading",
     "forbidden": "403",
     "unauthorized": "401",
+    "object" : "class",
+    "important" : "!important",
+    
 }
 
 const quotes = [
