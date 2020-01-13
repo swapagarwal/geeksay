0cd2387e1043889a0883ad0a56e181d238a4973f
Dominik Lech <teethtard321@gmail.com>
2020-01-13 16:26:08 +0000

    Added 5 new translations

--- geeksay.js
+++ geeksay.js
@@ -123,6 +123,11 @@ const translations = {
     "forbidden": "403",
     "unauthorized": "401",
     "nothing": "void",
+    "linux": "GNU/Linux",
+    "go": "cd",
+    "repeative": "recursive",
+    "database": "db",
+    "paragraph": "<p>",
 }
 
 const quotes = [
@@ -161,7 +166,7 @@ const quotes = [
     "I like to fix shit", // I like to debug dump
     "eat sleep code repeat", // eat shutdown code repeat
     "roses are red, violets are blue", // roses are ##ff0000, violets are ##0000ff
-    "The greatest trick the devil ever pulled was convincing the world he didn't exist", // The greatest trick the devil ever pulled was convincing the 0.0.0.0/0 he didn't exist 
+    "The greatest trick the devil ever pulled was convincing the world he didn't exist", // The greatest trick the devil ever pulled was convincing the 0.0.0.0/0 he didn't exist
     "home sweet home", // 127.0.0.1 sweet 127.0.0.1
     "history repeats itself", // log repeats itself
     "My dog likes to greet me at the door.", // My dog likes to ping me at the door.
