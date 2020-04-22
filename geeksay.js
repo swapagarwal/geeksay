bfa1d62185ab8b5a719c1e656cf396c5773c17e8
Tarun Sharma <47457466+ME-ON1@users.noreply.github.com>
2020-04-22 19:29:32 +0530

    added quote
    added a quote on this

--- geeksay.js
+++ geeksay.js
@@ -170,6 +170,7 @@ const quotes = [
     "Easy as pie", // easy as π
     "Live, laugh, love", // live, laugh, <3
     "Green with envy", // #00ff00 with envy
+    "Go, Corona Go" // 
 ]
 
 function isNumeric(num) {
