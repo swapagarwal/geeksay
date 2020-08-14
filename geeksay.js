144434494b02d2ac823dbe75f97606b0fdfc717e
Swarali Joshi <swarajos@cisco.com>
2020-08-14 18:36:54 +0530

    Added quote

--- geeksay.js
+++ geeksay.js
@@ -169,6 +169,7 @@ const quotes = [
     "Easy as pie", // easy as π
     "Live, laugh, love", // live, laugh, <3
     "Green with envy", // #00ff00 with envy
+    "I see red", //I see #ff0000
 ]
 
 function isNumeric(num) {
