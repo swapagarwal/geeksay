064b133f03b3a520de23d1074cb033c4ae97703b
ZITANIMohamed98 <60824720+ZITANIMohamed98@users.noreply.github.com>
2020-10-03 18:48:00 +0100

    update

--- geeksay.js
+++ geeksay.js
@@ -122,6 +122,8 @@ const translations = {
     "waiting": "loading",
     "unauthorized": "401",
     "nothing": "void",
+    "everyone":"*",
+    "was spoke freindly was you're friend":"(spokefriendly ? 'was you're friend' : 'was just acting)",
 }
 
 const quotes = [
@@ -160,7 +162,7 @@ const quotes = [
     "I like to fix shit", // I like to debug dump
     "eat sleep code repeat", // eat shutdown code repeat
     "roses are red, violets are blue", // roses are ##ff0000, violets are ##0000ff
-    "The greatest trick the devil ever pulled was convincing the world he didn't exist", // The greatest trick the devil ever pulled was convincing the 0.0.0.0/0 he didn't exist 
+    "The greatest trick the devil ever pulled was convincing the world he didn't exist", // The greatest trick the devil ever pulled was convincing the 0.0.0.0/0 he didn't exist
     "home sweet home", // 127.0.0.1 sweet 127.0.0.1
     "history repeats itself", // log repeats itself
     "My dog likes to greet me at the door.", // My dog likes to ping me at the door.
@@ -169,6 +171,7 @@ const quotes = [
     "Easy as pie", // easy as π
     "Live, laugh, love", // live, laugh, <3
     "Green with envy", // #00ff00 with envy
+    "not everyone who spoke you friendly was really your friend",// ! * (spokefriendly ? 'was you're friend' : 'was just acting)
 ]
 
 function isNumeric(num) {
