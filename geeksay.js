5b743c8f02f6f06e8e4dfda241a9247da989624e
Liam Hayden <l.hayden67@outlook.com>
2020-10-05 14:13:50 +0100

    Add a quote to geeksay.js

--- geeksay.js
+++ geeksay.js
@@ -169,6 +169,7 @@ const quotes = [
     "Easy as pie", // easy as π
     "Live, laugh, love", // live, laugh, <3
     "Green with envy", // #00ff00 with envy
+    "Once in a blue moon", //Once in a #0000ff moon
 ]
 
 function isNumeric(num) {
