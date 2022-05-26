48b95c5e9ee1ee5fa01049fffa5c00ac1ba03371
Anshaj Khare <anshaj13.ugcs20@iiitranchi.in>
2022-05-26 20:48:48 +0530

    Added some qoutes

--- geeksay.js
+++ geeksay.js
@@ -169,6 +169,8 @@ const quotes = [
     "Easy as pie", // easy as π
     "Live, laugh, love", // live, laugh, <3
     "Green with envy", // #00ff00 with envy
+    "you yourself are the anchor of your life", //you yourself are the <a> of your life
+    "you are the love of my life", //you are the <3 of my 42
 ]
 
 function isNumeric(num) {
