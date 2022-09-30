c7797c412873ae741a67ecc1dc293c2ad7c3df07
Dhyey Kathiriya <69355571+dk3775@users.noreply.github.com>
2022-09-30 23:33:59 +0530

    Formatting the main test file code (#360)
    * Formatting geeksay.js with prettier.
    
    * Formatting index.html as per standards.
    
    * Formatted test search file

--- test/geeksay.test.js
+++ test/geeksay.test.js
@@ -1,77 +1,87 @@
-const should = require('should');
-const esprima = require('esprima');
-const fs = require('fs');
-const geeksay = require('../geeksay')
+const should = require("should");
+const esprima = require("esprima");
+const fs = require("fs");
+const geeksay = require("../geeksay");
 
-describe('words', () => {
-  it('love', () => {
-    should.equal(geeksay('love'), '<3');
+describe("words", () => {
+  it("love", () => {
+    should.equal(geeksay("love"), "<3");
   });
 
-  it('not', () => {
-    should.equal(geeksay('not'), '!');
+  it("not", () => {
+    should.equal(geeksay("not"), "!");
   });
 
-  it('slow', () => {
-    should.equal(geeksay('slow'), 'O(n^n)');
+  it("slow", () => {
+    should.equal(geeksay("slow"), "O(n^n)");
   });
 
-  it('SloW - case insensitive', () => {
-    should.equal(geeksay('SloW'), 'O(n^n)');
-  });
-});
-
-
-describe('quotes', () => {
-  it('Hello World', () => {
-    should.equal(geeksay('Hello World'), 'ping 0.0.0.0/0');
-  });
-
-  it('heLLo WorlD - case insensitive', () => {
-    should.equal(geeksay('heLLo WorlD'), 'ping 0.0.0.0/0');
-  });
-
-  it('I love my house', () => {
-    should.equal(geeksay('I love my house'), 'I <3 my 127.0.0.1');
+  it("SloW - case insensitive", () => {
+    should.equal(geeksay("SloW"), "O(n^n)");
   });
 });
 
-describe('multi-line and symbols', () => {
+describe("quotes", () => {
+  it("Hello World", () => {
+    should.equal(geeksay("Hello World"), "ping 0.0.0.0/0");
+  });
+
+  it("heLLo WorlD - case insensitive", () => {
+    should.equal(geeksay("heLLo WorlD"), "ping 0.0.0.0/0");
+  });
+
+  it("I love my house", () => {
+    should.equal(geeksay("I love my house"), "I <3 my 127.0.0.1");
+  });
+});
+
+describe("multi-line and symbols", () => {
   it(`Every day after waking up \\n I say Hello World and \\n hi \\n to the world `, () => {
-    should.equal(geeksay(`Every day after waking up
+    should.equal(
+      geeksay(`Every day after waking up
         I say Hello World and 
         hi
-        to the world `), `Every day after waking up
+        to the world `),
+      `Every day after waking up
         I log ping 0.0.0.0/0 && 
         hi
-        to the 0.0.0.0/0 `);
+        to the 0.0.0.0/0 `
+    );
   });
 
-  it('space', () => {
-    should.equal(geeksay('I love my            house'), 'I <3 my            127.0.0.1');
+  it("space", () => {
+    should.equal(
+      geeksay("I love my            house"),
+      "I <3 my            127.0.0.1"
+    );
   });
 
-  it('symbols #1', () => {
-    should.equal(geeksay('I love my @ house'), 'I <3 my @ 127.0.0.1');
+  it("symbols #1", () => {
+    should.equal(geeksay("I love my @ house"), "I <3 my @ 127.0.0.1");
   });
 
-  it('symbols #2', () => {
-    should.equal(geeksay('(=> I love my house )'), '(=> I <3 my 127.0.0.1 )');
+  it("symbols #2", () => {
+    should.equal(geeksay("(=> I love my house )"), "(=> I <3 my 127.0.0.1 )");
   });
 
-  it('symbols #3', () => {
-  	should.equal(geeksay('I love my house!'), 'I <3 my 127.0.0.1!');
-  });
-  
-  it('symbols #4', () => {
-  	should.equal(geeksay('Roses are red, violets are blue.'), 'Roses are #ff0000, violets are #0000ff.');
+  it("symbols #3", () => {
+    should.equal(geeksay("I love my house!"), "I <3 my 127.0.0.1!");
   });
 
+  it("symbols #4", () => {
+    should.equal(
+      geeksay("Roses are red, violets are blue."),
+      "Roses are #ff0000, violets are #0000ff."
+    );
+  });
 });
 
-describe('html tags', () => {
-  it('inside tag with space', () => {
-    should.equal(geeksay('<a href="#"> Hello World </a>'), '<a href="#"> ping 0.0.0.0/0 </a>');
+describe("html tags", () => {
+  it("inside tag with space", () => {
+    should.equal(
+      geeksay('<a href="#"> Hello World </a>'),
+      '<a href="#"> ping 0.0.0.0/0 </a>'
+    );
   });
 
   // TODO:
@@ -80,46 +90,50 @@ describe('html tags', () => {
   // });
 });
 
-describe('inputs', () => {
-  it('input as array', () => {
-    should.equal(geeksay(['hello', 'my', 'love']), 'ping my <3');
+describe("inputs", () => {
+  it("input as array", () => {
+    should.equal(geeksay(["hello", "my", "love"]), "ping my <3");
   });
 
-  it('null', () => {
-    should.equal(geeksay(null), 'null');
+  it("null", () => {
+    should.equal(geeksay(null), "null");
   });
 
-  it('undefined', () => {
-    should.equal(geeksay(undefined), 'undefined');
+  it("undefined", () => {
+    should.equal(geeksay(undefined), "undefined");
   });
 
-  it('object', () => {
-    should.equal(geeksay({ Hello: 'World' }), '[object Object]');
+  it("object", () => {
+    should.equal(geeksay({ Hello: "World" }), "[object Object]");
   });
 
-  it('array of numbers', () => {
-    should.equal(geeksay([1, 2, 3, 4, '100']), '1 10 11 100 1100100');
+  it("array of numbers", () => {
+    should.equal(geeksay([1, 2, 3, 4, "100"]), "1 10 11 100 1100100");
   });
 });
 
-describe('code checks', () => {
-  it('duplicate translation entries', () => {
-    const libfile = '../geeksay.js'    // Library file location
-    const varname = 'translations'     // Variable to check for duplicate entries
+describe("code checks", () => {
+  it("duplicate translation entries", () => {
+    const libfile = "../geeksay.js"; // Library file location
+    const varname = "translations"; // Variable to check for duplicate entries
 
-    var code = fs.readFileSync(__dirname + '/' + libfile, 'utf8')
-    var ast  = esprima.parse(code);
+    var code = fs.readFileSync(__dirname + "/" + libfile, "utf8");
+    var ast = esprima.parse(code);
 
     // Find 'translations' variable and extract entries' keys
     var duplicates;
     for (const i in ast.body) {
-      let decl = ast.body[i]
-      if (decl.type == 'VariableDeclaration') {
+      let decl = ast.body[i];
+      if (decl.type == "VariableDeclaration") {
         for (const j in decl.declarations) {
           if (decl.declarations[j].id.name == varname) {
             translations = decl.declarations[j];
-            entries      = translations.init.properties.map(prop => prop.key.value);
-            duplicates   = entries.filter((item, index) => entries.indexOf(item) != index);
+            entries = translations.init.properties.map(
+              (prop) => prop.key.value
+            );
+            duplicates = entries.filter(
+              (item, index) => entries.indexOf(item) != index
+            );
           }
         }
       }
@@ -127,12 +141,23 @@ describe('code checks', () => {
 
     // Case if 'translations' variable was not found and 'duplicates' is therefore undefined
     if (typeof duplicates == "undefined") {
-      throw Error("Variable \'" + varname + "\' was not found in library file (" + libfile + ").");
+      throw Error(
+        "Variable '" +
+          varname +
+          "' was not found in library file (" +
+          libfile +
+          ")."
+      );
     }
 
     // The number of duplicates should be 0
-    should.equal(duplicates.length, 0, 
-      "Found duplicate " + duplicates.length + " entries: " + duplicates.map(entry => '"'+entry+'"').join(', ') );
-
+    should.equal(
+      duplicates.length,
+      0,
+      "Found duplicate " +
+        duplicates.length +
+        " entries: " +
+        duplicates.map((entry) => '"' + entry + '"').join(", ")
+    );
   });
 });
