d72c18dcaa0146654d2b7b50008f08c7c22b7d41
Febi Arifin <62456215+febiarifin@users.noreply.github.com>
2022-10-03 11:24:13 +0700

    Added 4 quotes (#378)
    Co-authored-by: Dhyey Kathiriya <69355571+dk3775@users.noreply.github.com>

--- geeksay.js
+++ geeksay.js
@@ -205,6 +205,10 @@ const quotes = [
   "hide it!", //incognito it!
   "99 little bugs in the code. 99 little bugs in the code. Take one down, patch it around. 127 little bugs in the code", //cb - so many bugs
   "Hey Google! Find me the best repo.", //ping Google! ctrl+f self the best repo
+  "Paste my Code", // ctrl+v my Code
+  "Paste my Copy", // ctrl+v my ctrl+c
+  "What's new?", // What's ctrl+n
+  "You're done coding! Go to sleep." // You're done coding! Go to shutdown
   "Life is a big question that even Google can't find answer.",//42 is a big ? that even Google can't ctrl+f answer
   "Have no friends not equal to yourself.",// Have no friends ! = to yourself
   "The past does not equal the future.",//The past does ! = the future
