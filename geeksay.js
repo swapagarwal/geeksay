cc9c1bcd7ba871411d5a3b39aece4e9581f38aad
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2022-10-03 08:08:34 +0500

    Update geeksay.js (first contribution))
    Just added a quote after sync fork.

--- geeksay.js
+++ geeksay.js
@@ -198,6 +198,7 @@ const quotes = [
   "Life is a big question that even Google can't find answer.",//42 is a big ? that even Google can't ctrl+f answer
   "Have no friends not equal to yourself.",// Have no friends ! = to yourself
   "The past does not equal the future.",//The past does ! = the future
+  "I just switch tabs, not the love ones",// I just alt+tab tabs, not the <3 ones
 ];
 
 function isNumeric(num) {
