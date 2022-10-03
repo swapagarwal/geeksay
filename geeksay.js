4efaaf4038d3c908c7aa44c5cdbb75055546a629
Anshaj Khare <anshaj13.ugcs20@iiitranchi.ac.in>
2022-10-03 21:22:23 +0530

    Changes Done Again

--- geeksay.js
+++ geeksay.js
@@ -126,7 +126,8 @@ const translations = {
     "unauthorized": "401",
     "nothing": "void",
     "pink": "#ffc0cb",
-    "developer": "dev",
+    "style": "CSS", 
+    "developer": "dev"
 };
 
 const quotes = [
@@ -165,7 +166,7 @@ const quotes = [
     "I like to fix shit", // I like to debug dump
     "eat sleep code repeat", // eat shutdown code repeat
     "roses are red, violets are blue", // roses are ##ff0000, violets are ##0000ff
-    "The greatest trick the devil ever pulled was convincing the world he didn't exist", // The greatest trick the devil ever pulled was convincing the 0.0.0.0/0 he didn't exist
+    "The greatest trick the devil ever pulled was convincing the world he didn't exist", // The greatest trick the devil ever pulled was convincing the 0.0.0.0/0 he didn't exist 
     "home sweet home", // 127.0.0.1 sweet 127.0.0.1
     "history repeats itself", // log repeats itself
     "My dog likes to greet me at the door.", // My dog likes to ping me at the door.
