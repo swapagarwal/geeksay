b30d664b21ab814a88322818bc6339e5b6973d01
Shifa25 <shifashaikh2505@gmail.com>
2023-10-30 22:46:25 +0530

    added quotes in geeksay.js

--- geeksay.js
+++ geeksay.js
@@ -274,6 +274,8 @@ const quotes = [
   "Sometimes it's okay to run", // Sometimes it's okay to ctrl+F5
   "Divide and conquer", // / and conquer
   "It's nice to find love, propose, send gifts, marry and live happy life", // It's nice to ctrl+f <3, commit, push gifts, merge && live :) 42
+  "He's so persistent", // He's so while(true)
+  "She's quite dynamic", //She's quite JavaScript. 
 ];
 
 function isNumeric(num) {
